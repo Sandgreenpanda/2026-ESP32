06dadafd1e0c29530e833be1478dddfcea7d0c4a
Sandgreenpanda <23123@wc.school.nz>
2026-06-26 08:49:26 +1200

    fixed many visual and config issues

---
+++ extra_script.py
@@ -0,0 +1,5 @@
+# A script for helping with errors in vscode red underlines (I did not make this, this fix comes from online)
+
+import os
+Import("env")
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)

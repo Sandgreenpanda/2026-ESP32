2e3de9cf85e1b0b11bc9562483edc381c986690f
Sandgreenpanda <23123@wc.school.nz>
2026-06-29 08:42:12 +1200

    HTTPS!

---
+++ .pio/build/esp32doit-devkit-v1/liba78/esp32_https_server/HTTPValidator.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/liba78/esp32_https_server/HTTPValidator.cpp.o: \
+ .pio/libdeps/esp32doit-devkit-v1/esp32_https_server/src/HTTPValidator.cpp \
+ .pio/libdeps/esp32doit-devkit-v1/esp32_https_server/src/HTTPValidator.hpp

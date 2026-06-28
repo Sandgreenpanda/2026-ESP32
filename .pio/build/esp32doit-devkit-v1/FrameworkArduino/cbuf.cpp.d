4ca2980ae1ccfb05ac2d01e909fc401de3c9c3ba
sandgreenpanda <23123@wc.school.nz>
2026-06-29 08:56:57 +1200

    I hope this doesnt del bad

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
- /home/alext/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/alext/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/libb64/cdecode.c.o: \
- /home/alext/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- /home/alext/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
